bc5c5e8e24c911b44f1425e35d95e252d09f56de
Stefan Oravec <stefan.oravec@me.com>
2017-11-21 15:09:41 +0100

    init

---
+++ package.sql
@@ -0,0 +1,8 @@
+rem Package name
+define g_package_name="TRADAA"
+
+rem SQL name compatible version string
+define g_sql_version = "000100"
+
+rem full semver version string
+define g_semver_version = "0.1.0"
